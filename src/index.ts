b3b7916ff0b8bcf1b207ccb83aba9c48de432152
iodar <granzowdario@gmail.com>
2019-11-29 21:10:00 +0100

    repo(mocha test ui): added config for mocha test ui

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import find from "local-devices"
 import { createMarkdown } from "./exporter/markdownExporter"
 
 find().then((devices) => {
-    const networkDevices = devices
-    const markdown = createMarkdown(networkDevices)
-    markdown.forEach((entry) => {
-        console.log(`${entry}`)
-    })
+    // const networkDevices = devices
+    // const markdown = createMarkdown(networkDevices)
+    // markdown.forEach((entry) => {
+    //     console.log(`${entry}`)
+    // })
+    console.log(devices)
 })
